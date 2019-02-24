dd926fb4bebf0c7529f2a22ee728b4e39c402e6d
Kingsolomon Ifeanyi <solokinginbox@gmail.com>
2019-02-24 05:57:27 +0100

    display mock data to components

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -30,8 +30,8 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			}
+				use: ['style-loader', 'css-loader', 'postcss-loader'],
+			},
 		]
 	},
 	resolve: {
